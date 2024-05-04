ff9351042fc686871ce362a29bfe798ad7410ec8
Wilson Juma <wjuma19dev@gmail.com>
2024-05-04 13:52:04 -0400

    Adding active to navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,11 +4,4 @@ import { Component, EventEmitter, Output } from "@angular/core";
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-
-    @Output() featureSelected = new EventEmitter();
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
-}
+export class HeaderComponent {}
